0c71a9a7642e885e8b29e7cf31ceb383bc019566
Kawish Zia <kawishzia2826@gmail.com>
2024-11-01 05:30:28 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,129 @@
+
+
+
+//==========================================================Assignment No   (11)======================================================================================
+// 1. Declare and initialize an empty multidimensional array.
+// (Array of arrays)
+// var multi = [[],[],[]];
+// 2. Declare and initialize a multidimensional array
+// representing the following matrix:
+// var multi = [[0],[1],[2],[3]];
+// document.write([0][0]);
+// document.write([1][0]);
+// document.write([2][0]);
+// document.write([3][0] + "<br/>");
+
+// document.write([1][0]);
+// document.write([0][0]);
+// document.write([1][0]);
+// document.write([2][0] + "<br/>");
+
+// document.write([2][0]);
+// document.write([1][0]);
+// document.write([0][0]);
+// document.write([1][0] + "<br/>");
+
+// 3. Write a program to print numeric counting from 1 to 10.
+// for(var i=0; i<10; i++){
+//         document.write(i +  "<br/>");
+// }
+
+// 4. Write a program to print multiplication table of any
+// number using for loop. Table number & length should be
+// taken as an input from user.
+
+// var userinput =+prompt("Please enter a number");
+// var interlength = +prompt("Please enter");
+// for(var i=1; i<=interlength; i++){
+//         document.write(userinput + "x" + i + "=" + userinput*i + "<br/>")
+// };
+
+// 5. Write a program to print items of the following array
+// using for loop:
+// fruits = [“apple”, “banana”, “mango”, “orange”,
+// “strawberry”]
+
+// var fruits = ["“apple”", "“banana”", "“mango”", "“orange”","“strawberry”"];
+// for(var i = 0; i < fruits.length; i){
+// console.log(fruits[i]);
+// }
+
+
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+// for (var i = 0; i < fruits.length; i++) {
+//     document.write(fruits[i] + "<br/>");
+
+    
+    
+// }
+
+// 6. Generate the following series in your browser. See example output. a. Counting: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 b. Reverse counting: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 c. Even: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 d. Odd: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 e. Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
+    
+// Page 5 of 7
+// 7. You have an array A = [“cake”, “apple pie”, “cookie”, “chips”, “patties”] Write a program to enable “search by user input” in an array. After searching, prompt the user whether the given item is found in the list or not. Example:
+
+// 7. You have an array A = [“cake”, “apple pie”, “cookie”, “chips”, “patties”] Write a program to enable “search by user input” in an array. After searching, prompt the user whether the given item is found in the list or not. Example:
+
+
+
+
+// var A = ["cake", "apple" ,"pie", "cookie", "chips", "patties"];
+// var userCity = prompt("Enter your city");
+// var flag = false;
+
+// for (var i = 0; i < A.length; i++) {
+//   if (A[i] === userCity) {
+//     flag = true;
+//     break;
+//   }
+// }
+
+// if (flag) {
+//   alert("true");
+// } else {
+//   alert("false");
+// }
+
+// for(var i = 0; i < 10; i++){
+//     console.log(i);
+//     if (i === 5) {
+//         break;
+//     }
+// }
+// qno8
+ 
+// var num = [24, 53, 78, 91, 12];
+
+// console.log(A[4]);
+
+// q 09
+ 
+// var num = [24, 53, 78, 91, 12];
+
+// console.log(A[0]);
+   
+// q 10 
+//  var i;
+// for (i = 1; i <= 100; i++) {
+  
+//     if (i % 5 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// let name1  = prompt("");
+// let firstMethod = name1.split("");
+// console.log(firstMethod);
+// let secondMethod = firstMethod.reverse();
+// console.log(secondMethod);
+// let thirdMethod = secondMethod.join("");
+// console.log(thirdMethod);
+// let result = name1.toLowerCase();
+// let result2  = thirdMethod.toLowerCase();
+// if (result === result2){
+//     alert("its concentination avilable ");
+// }
+// else {
+//     alert("nothing to do");
